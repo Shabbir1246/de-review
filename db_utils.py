553fe5f62cb28cca4ba105994ec6e1f45e8085e3
Verity Gregory <verity.gregory@gmail.com>
2023-01-26 10:20:22 +0000

    create util files to connect to draft database

--- db_utils.py
+++ db_utils.py
@@ -1,8 +1,26 @@
-# def get_items():
-#     return column_name, rows
+import pg8000.native as pg
 
-# def get_staff():
-#     return column_name, rows
+# change the variable below to your psql username. Don't forget to add your password if needed.
+DB_USERNAME = "verity"
+DB_NAME = "nc_sells_fridges_draft"
+conn = pg.Connection(DB_USERNAME,database=DB_NAME)
 
-# def get_sales():
-#     return column_name, rows
+def get_items():
+    query = 'SELECT * FROM items'
+    rows = conn.run(query)
+    column_names = [col['name']for col in conn.columns]
+
+    return rows, column_names
+
+def get_staff():
+    query = 'SELECT * FROM staff'
+    rows = conn.run(query)
+    column_names = [col['name']for col in conn.columns]
+    return rows, column_names
+
+def get_sales():
+    query = 'SELECT * FROM sales'
+    rows = conn.run(query)
+    column_names = [col['name']for col in conn.columns]
+    return rows, column_names
+
