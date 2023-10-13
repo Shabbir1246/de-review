d483cf8a0780809104c4277a563eea84c0b36ea3
Shabbir1246 <mydatabackupac@gmail.com>
2023-10-14 00:49:38 +0100

    completes task3 of section 1

--- src/section1/task3.py
+++ src/section1/task3.py
@@ -1,6 +1,11 @@
 def format_features(stock_dict):
 
-    features_list = []
-    features_list = [stock_item.get('features', 'Key not supplied') for stock_item in stock_dict]
-
-    return features_list
+    # using list comprehension loop on list to find individual stock item 
+    # and then loop again on features key to seperate out individual features
+    features_list = [[feature] for stock_item in stock_dict for feature in stock_item.get('features', '')]
+    if len(features_list) == 1:
+        return features_list
+    else:
+        # remove any repeate feature in the list of lists of features 
+        unique_features_list = [features_list[i] for i in range(len(features_list)) if features_list[i] not in features_list[i + 1:]]
+        return unique_features_list
--- test/test_task3.py
+++ test/test_task3.py
@@ -6,22 +6,33 @@ def test_should_take_a_list_of_dicts_and_return_an_empty_list_if_no_items_in_dic
     assert output1  == []
 
 def test_should_return_an_empty_entry_in_the_list_if_features_key_was_not_found():
-    output1 = format_features([{}])
-    assert output1  == ['Key not supplied']
+    output1 = format_features(
+        [{'item_name': 'Louboutin Flip Flops',
+        'department': 'Footwear',
+        'amount_in_stock': 5}])
+    assert output1  == []
 
-
-def test_should_take_a_list_of_dicts_and_return_a_list_of_lists_containing_feature_name():
+def test_should_take_a_list_of_dicts_and_return_a_list_of_lists_containing_feature():
     output2 = format_features([
+    {
+        'item_name': 'Louboutin Flip Flops',
+        'features': ['Designer'],
+        'department': 'Footwear',
+        'amount_in_stock': 5
+    }])
+    assert output2 ==  [['Designer']]
+
+    output3 = format_features([
     {
         'item_name': 'Louboutin Flip Flops',
         'features': ['Designer', 'Faux-Faux-Leather'],
         'department': 'Footwear',
         'amount_in_stock': 5
     }])
-    assert output2 ==  [['Designer', 'Faux-Faux-Leather']]
+    assert output3 ==  [['Designer'], ['Faux-Faux-Leather']]
 
-def test_should_take_a_list_of_dicts_and_return_a_list_of_lists_containing_unique_features():
-    output3 = format_features([
+def test_should_take_a_list_of_dicts_and_return_a_list_of_lists_each_containing_unique_features():
+    output4 = format_features([
     {
         'item_name': 'Louboutin Flip Flops',
         'features': ['Designer', 'Faux-Faux-Leather'],
@@ -33,6 +44,19 @@ def test_should_take_a_list_of_dicts_and_return_a_list_of_lists_containing_uniqu
         'department': 'Beauty',
         'amount_in_stock': 10
     }])
-    assert output3 ==  [['Designer'], ['Faux-Faux-Leather'], ['Fragrance']]
+
+    # returned result is a list
+    assert type(output4) == type([])
+    # every element within the holding list is a list
+    for feature in output4:
+        assert type(feature) == type([])
+
+    # each list within the holding list contains a single feature only
+    for feature in output4:
+        assert len(feature) == 1
+    
+    # list of list is made up of unique features from each stock item features
+    assert output4 ==  [['Faux-Faux-Leather'], ['Fragrance'], ['Designer']]
+
 
 
